e02b1d704b232cd183ce9b652e26ae3fca49c0a7
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-09-23 15:14:45 -0500

    dec_tree_test_data

--- assignment2.py
+++ assignment2.py
@@ -7,25 +7,30 @@ import sklearn.tree as tree
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+# read in data
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3.csv")
+test_data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3test.csv")
 
-# drop string and datetime columns
+# clean data
 columns_drop = ['id','DateTime']
 data = data.drop(columns_drop, axis=1)
 
-# Separate x and y variables
+test_columns_drop = ['id','DateTime']
+test_data = test_data.drop(test_columns_drop, axis=1)
+
+# data
 Y = data['meal']
 X = data.drop('meal', axis = 1)
+yt = test_data['meal']
+xt = test_data.drop('meal', axis = 1)
 
-# Initializing the model
-model = tree.DecisionTreeClassifier()
 
-# Fit model
+model = tree.DecisionTreeClassifier(max_depth=5, min_samples_leaf = 10)
+
 modelFit = model.fit(X,Y)
 
 # Make predictions
-forecast = modelFit.predict(X)
+forecast = modelFit.predict(xt)
 
 # convert to list and make proper length
 pred = forecast.tolist()
-pred = pred[:1000]
