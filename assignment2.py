4e0dabdf9f72c172df2f9f2833c4e8ada9201103
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-09-22 19:51:53 -0500

    dec_tree_v1

---
+++ assignment2.py
@@ -0,0 +1,27 @@
+# Our import statements for this problem
+import pandas as pd
+import numpy as np
+import patsy as pt
+
+import sklearn.tree as tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3.csv")
+
+# drop string and datetime columns
+columns_drop = ['id','DateTime']
+data = data.drop(columns_drop, axis=1)
+
+# Separate x and y variables
+Y = data['meal']
+X = data.drop('meal', axis = 1)
+
+# Initializing the model
+model = tree.DecisionTreeClassifier()
+
+# Fit model
+modelFit = model.fit(X,Y)
+
+# Make predictions
+pred = modelFit.predict(X)
