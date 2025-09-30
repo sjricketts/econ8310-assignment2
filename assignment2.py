da15e064bc39afebec2b07ff86db8521c6cbd2ab
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-09-30 14:29:15 -0500

    rf_v1.2

--- assignment2.py
+++ assignment2.py
@@ -1,11 +1,7 @@
 # Our import statements for this problem
 import pandas as pd
 import numpy as np
-import patsy as pt
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 
 # read in data
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3.csv")
@@ -19,16 +15,16 @@ test_columns_drop = ['id','DateTime']
 test_data = test_data.drop(test_columns_drop, axis=1)
 
 # data
-Y = data['meal']
-X = data.drop('meal', axis = 1)
+y = data['meal']
+x = data.drop('meal', axis = 1)
 yt = test_data['meal']
 xt = test_data.drop('meal', axis = 1)
 
 # generate random forest model
-model = RandomForestClassifier(n_estimators=100, n_jobs = -1, max_depth=10)
+model = RandomForestClassifier(n_estimators=100, n_jobs = -1, max_depth=10, random_state=42)
 
 # fit model
-modelFit = model.fit(X,Y)
+modelFit = model.fit(x,y)
 
 # Make predictions
 forecast = modelFit.predict(xt)
