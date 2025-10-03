0ce51a592443b784e5f0297b014b5cc53f719cb5
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-10-02 19:40:05 -0500

    boosted_v1

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,6 @@
-# Our import statements for this problem
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 # read in data
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3.csv")
@@ -15,18 +14,18 @@ test_columns_drop = ['id','DateTime']
 test_data = test_data.drop(test_columns_drop, axis=1)
 
 # data
-y = data['meal']
-x = data.drop('meal', axis = 1)
+Y = data['meal']
+X = data.drop('meal', axis = 1)
 yt = test_data['meal']
 xt = test_data.drop('meal', axis = 1)
 
-# generate random forest model
-model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+# model
+model = XGBClassifier(n_estimators=15, max_depth=3,learning_rate=0.5, objective='binary:logistic', random_state=42)
 
-# fit model
-modelFit = model.fit(x,y)
+# fit
+modelFit = model.fit(X, Y)
 
-# Make predictions
+# Make predictions based on the testing x values
 forecast = modelFit.predict(xt)
 
 # convert to list
